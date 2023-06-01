23c2756a33be6b4b99c84911f29b43dd789b8f40
GFTD WORKS <gftdworks@GFTDs-MacBook-Pro.local>
2023-06-01 22:01:58 +0900

    環境変数の設定

--- shared/settings.py
+++ shared/settings.py
@@ -8,6 +8,8 @@ dotenv_path = join(dirname(__file__), '.env')
 load_dotenv(dotenv_path)
 
 # LINE
-LINE_CHANNEL_SECRET = os.environ.get("LINE_CHANNEL_SECRET")
-LINE_CHANNEL_ACCESS_TOKEN = os.environ.get("LINE_CHANNEL_ACCESS_TOKEN")
+LINE_CHANNEL_SECRET = os.environ.get(
+    "LINE_CHANNEL_SECRET", os.getenv("LINE_CHANNEL_SECRET"))
+LINE_CHANNEL_ACCESS_TOKEN = os.environ.get(
+    "LINE_CHANNEL_ACCESS_TOKEN", os.getenv("LINE_CHANNEL_ACCESS_TOKEN"))
 ENV = os.environ.get("ENV")
